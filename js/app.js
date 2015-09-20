8eb066343cfbd40f18cf63ed5ca03956753e2dad
Jack Jordan <jack023@live.com.au>
2015-09-20 20:14:58 +1000

    Removed random circle.
    Removed the random circle from the bottom, resized cells to better fit
    sublists.

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@ var optionsWidth = $(".options").width();
 
 var listTop = editorHeight/10;
 var listCenterX = editorWidth/2 + optionsWidth;
-var cellSize = 80; //both width and height
+var cellSize = 60; //both width and height
 
 for (i = 0; i < 8; i++)
 {
@@ -23,22 +23,27 @@ for (i = 0; i < 8; i++)
 }
 
 
+for (i = 0; i < 4; i++)
+{
+  //i-5 is based off the fact that the we need it to be 4 left of center but we
+  //are working from the left side.
+  var newRect = s.rect(listCenterX/2 + (cellSize * (i-3)) , 3 * editorHeight / 8, cellSize, cellSize);
+  newRect.appendTo(list);
+}
+
+for (i = 0; i < 4; i++)
+{
+  //i-5 is based off the fact that the we need it to be 4 left of center but we
+  //are working from the left side.
+  var newRect = s.rect(3*listCenterX/2 + (cellSize * (i-3)) , 3 * editorHeight / 8, cellSize, cellSize);
+  newRect.appendTo(list);
+}
+
 // var rect1 = s.rect(editorWidth/5, editorHeight/10, 80, 80);
 // rect1.appendTo(list);
 // var rect2 = s.rect(editorWidth/5 + 80, editorHeight/10, 80, 80);
 // rect2.appendTo(list);
-// var rect3 = s.rect(editorWidth/5 + 80*2, editorHeight/10, 80, 80);
-// rect3.appendTo(list);
-// var rect4 = s.rect(editorWidth/5 + 80*3, editorHeight/10, 80, 80);
-// rect4.appendTo(list);
-// var rect5 = s.rect(editorWidth/5 + 80*4, editorHeight/10, 80, 80);
-// rect5.appendTo(list);
-// var rect6 = s.rect(editorWidth/5 + 80*5, editorHeight/10, 80, 80);
-// rect6.appendTo(list);
-// var rect7 = s.rect(editorWidth/5 + 80*6, editorHeight/10, 80, 80);
-// rect7.appendTo(list);
-// var rect8 = s.rect(editorWidth/5 + 80*7, editorHeight/10, 80, 80);
-// rect8.appendTo(list);
+
 // By default its black, lets change its attributes
 // mainList.attr({
 // "fill": "rgba(0,0,0,0)",
--- js/list.js
+++
@@ -1,3 +0,0 @@
-
-var paper = Snap(800, 600),
-    circle = paper.circle(100, 50, 10);
