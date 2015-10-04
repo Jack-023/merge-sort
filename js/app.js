722a67b5d4c63668a631d6fae995e3d0fed60598
Jack Jordan <jack023@live.com.au>
2015-10-04 22:22:11 +1100

    Now produces random list of draggable ints
    now draws 8 random draggable numbers in the range [1, 9].
    These are drawn above cells so they can always be dragged.
    Numbers are generated using a shuffle bag algorithm so there are no
    duplicate numbers.

--- js/app.js
+++ js/app.js
@@ -12,37 +12,51 @@ var listTop = editorHeight/10;
 var cellSize = editorWidth*0.040697674418604654; //both width and height
 //0.040697674418604654 is calulated from 70 cell size at my default size
 
+var allLists = [];
+
+var drawNumber = function(x, y, size, number) {
+  var testCell = s.rect(x, y, size, size);
+  testCell.attr('stroke-width', 0)
+  var testNumber = s.text(x + (cellSize / 2), y + (cellSize * 3 / 4), number);
+  testNumber.attr('font-size', cellSize * .8);
+  testNumber.attr('text-anchor', 'middle');
+
+  var testN = s.group(testCell, testNumber);
+  testN.drag(move, start, stop);
+
+}
+
 var drawList = function(center, length, size, depth) {
 
+  var newList = [];
   for (i = 0; i < length; i++)
   {
     //i-5 is based off the fact that the we need it to be 4 left of center but we
     //are working from the left side.
     var newRect = s.rect(center + (size * (i-(length/2))) , (depth-.5)*editorHeight/4, size, size);
-    newRect.appendTo(list);
+    newList.push(newRect);
   }
-
-
+  return newList;
 }
+var drawNumbers = function() {
 
-// var addEvent = function(object, type, callback) {
-//     if (object == null || typeof(object) == 'undefined') return;
-//     if (object.addEventListener) {
-//         object.addEventListener(type, callback, false);
-//     } else if (object.attachEvent) {
-//         object.attachEvent("on" + type, callback);
-//     } else {
-//         object["on"+type] = callback;
-//     }
-// };
-//
-// addEvent(window, "resize", updateWindow);
-//
-// function updateWindow(){
-//   list.forEach(function (item, index, array) {
-//     item.transform('SeditorWidth*0.040697674418604654');
-//   }
-// }
+  /**
+   * Randomize array element order in-place.
+   * Using Durstenfeld shuffle algorithm.
+   */
+ var ints = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (var i = ints.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = ints[i];
+        ints[i] = ints[j];
+        ints[j] = temp;
+    }
+
+  for (i = 0; i < 8; i++)
+  {
+    drawNumber(editorWidth/2 + optionsWidth + (cellSize * (i-(8/2))), (1-.5)*editorHeight/4, cellSize, ints[i]);
+  }
+}
 
 //the following three functions are for dragging
 var move = function(dx,dy) {
@@ -59,35 +73,28 @@ var stop = function() {
 }
 
 
-var testCell = s.rect(300, 100, cellSize, cellSize);
-var testNumber = s.text(300 + (cellSize * 1 /2), 100 + (cellSize * 3 / 4), "5");
-testNumber.attr('font-size', cellSize * .8);
-testNumber.attr('text-anchor', 'middle');
 
-//testNumber.attr('text-align', 'center');
+allLists.push(drawList(editorWidth/2 + optionsWidth, 8, cellSize, 1));
 
-var testN = s.group(testCell, testNumber);
+allLists.push(drawList(1*editorWidth/4 + optionsWidth, 4, cellSize, 2));
+allLists.push(drawList(3*editorWidth/4 + optionsWidth, 4, cellSize, 2));
 
-testN.drag(move, start, stop);
+allLists.push(drawList(1*editorWidth/8 + optionsWidth, 2, cellSize, 3));
+allLists.push(drawList(3*editorWidth/8 + optionsWidth, 2, cellSize, 3));
+allLists.push(drawList(5*editorWidth/8 + optionsWidth, 2, cellSize, 3));
+allLists.push(drawList(7*editorWidth/8 + optionsWidth, 2, cellSize, 3));
 
-testCell.attr('fill', 'rgba(255, 255, 255, 0)');
+allLists.push(drawList(1*editorWidth/16 + optionsWidth, 1, cellSize, 4));
+allLists.push(drawList(3*editorWidth/16 + optionsWidth, 1, cellSize, 4));
+allLists.push(drawList(5*editorWidth/16 + optionsWidth, 1, cellSize, 4));
+allLists.push(drawList(7*editorWidth/16 + optionsWidth, 1, cellSize, 4));
+allLists.push(drawList(9*editorWidth/16 + optionsWidth, 1, cellSize, 4));
+allLists.push(drawList(11*editorWidth/16 + optionsWidth, 1, cellSize, 4));
+allLists.push(drawList(13*editorWidth/16 + optionsWidth, 1, cellSize, 4));
+allLists.push(drawList(15*editorWidth/16 + optionsWidth, 1, cellSize, 4));
 
-
-drawList(editorWidth/2 + optionsWidth, 8, cellSize, 1);
-
-drawList(1*editorWidth/4 + optionsWidth, 4, cellSize, 2);
-drawList(3*editorWidth/4 + optionsWidth, 4, cellSize, 2);
-
-drawList(1*editorWidth/8 + optionsWidth, 2, cellSize, 3);
-drawList(3*editorWidth/8 + optionsWidth, 2, cellSize, 3);
-drawList(5*editorWidth/8 + optionsWidth, 2, cellSize, 3);
-drawList(7*editorWidth/8 + optionsWidth, 2, cellSize, 3);
-
-drawList(1*editorWidth/16 + optionsWidth, 1, cellSize, 4);
-drawList(3*editorWidth/16 + optionsWidth, 1, cellSize, 4);
-drawList(5*editorWidth/16 + optionsWidth, 1, cellSize, 4);
-drawList(7*editorWidth/16 + optionsWidth, 1, cellSize, 4);
-drawList(9*editorWidth/16 + optionsWidth, 1, cellSize, 4);
-drawList(11*editorWidth/16 + optionsWidth, 1, cellSize, 4);
-drawList(13*editorWidth/16 + optionsWidth, 1, cellSize, 4);
-drawList(15*editorWidth/16 + optionsWidth, 1, cellSize, 4);
+drawNumbers();
+// var testCell = s.rect(editorWidth/2 + optionsWidth, 8, cellSize, cellSize);
+// var testNumber = s.text(parseInt(testCell.attr("x"), 10) + (cellSize / 2), parseInt(testCell.attr('y'), 10) + (cellSize * 3 / 4), "5");
+// testNumber.attr('font-size', cellSize * .8);
+// testNumber.attr('text-anchor', 'middle');
